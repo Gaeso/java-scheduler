7b3281284d6c530b29804055b960d87741d657a2
gaeso <ghwns2239@gmail.com>
2025-05-19 16:15:55 +0900

    init : 프로젝트 최초 생성

---
+++ src/test/java/com/example/javascheduler/JavaSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javascheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
